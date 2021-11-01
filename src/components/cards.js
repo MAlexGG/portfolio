9567a9163fb3369b9d5d6256d02efaf7ee14971d
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-11-01 14:03:05 +0100

    add reverse() in iteration to read the json projects files bottom to top

--- src/components/cards.js
+++ src/components/cards.js
@@ -8,7 +8,7 @@ class Cards extends Component{
             <div className="ct-cards">
                 {this.props.data.projects.map((card, index) => (
                     <Card key={index} title={card.title} subtitle={card.subtitle} description={card.description} prototype={card.prototype}   website={card.website} repository={card.repository} index={index}></Card>
-                ))}
+                )).reverse()}
                 <EmptyCard/>
             </div>
         );
