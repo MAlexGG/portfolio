cb32a13ebda49d2de139623ab2d7f6c1da71e74a
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-11-06 15:48:12 +0100

    project page styles and methods

--- src/components/project.js
+++ src/components/project.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
+import close from "../assets/img/close.svg";
 
 class Project extends Component{
+
+    goBack = e => {
+        e.preventDefault();
+        this.props.history.goBack();
+    }
+
     render(){
         const {data} = this.props.location;
         const loadImage = image => (require(`../assets/img/ui/${data.img}`).default);
         return(
             <div className="ct-project">
+                <button className="bt-ico bt-projectClose" onClick={this.goBack}><img className="img-ico" src={close} alt="close button" /></button>
                 <div className="ct-projectImg">
-                    <img className="" src={loadImage("image.jpg")} alt="design UI"/> 
+                    <img className="img-projectImg" src={loadImage("image.jpg")} alt="design UI"/> 
                 </div>
-                <div>
-                    <h2>{data.title}</h2>
-                    <p>{data.description}</p>
+                <div className="ct-projectTxt">
+                    <h2 className="txt-projectTitle">{data.title}</h2>
+                    <p className="txt-projectDesc">{data.description}</p>
                 </div>
             </div>
         );
