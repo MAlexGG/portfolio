ad6a6104a180e1c16eb58180c8e6a3b3923c6053
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-11-07 13:47:22 +0100

    basename inserted in browser router

--- src/components/router.js
+++ src/components/router.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "../App";
 import Projects from "./projects";
 import Tools from "./tools";
@@ -12,21 +12,42 @@ import Project from "./project";
 import NotFound from "./notFound";
 
 
-const Router = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={App} />
-            <Route path="/aboutme" component={Aboutme} />
-            <Route path="/projects" component={Projects} />
-            <Route path="/pedagogical" component={Pedagogical} />
-            <Route path="/pedagogical-projects" component={PedagogicalProjects} />
-            <Route path="/professional" component={Professional} />
-            <Route path="/professional-projects" component={ProfessionalProjects} />
-            <Route exact path="/project/:index" component={Project}/>
-            <Route path="/tools" component={Tools} />
-            <Route component={NotFound}/>
-        </Switch>
-    </BrowserRouter>
+const Router = () => ( <
+    BrowserRouter basename = "/portfolio/" >
+    <
+    Switch >
+    <
+    Route exact path = "/"
+    component = { App }
+    /> <
+    Route path = "/aboutme"
+    component = { Aboutme }
+    /> <
+    Route path = "/projects"
+    component = { Projects }
+    /> <
+    Route path = "/pedagogical"
+    component = { Pedagogical }
+    /> <
+    Route path = "/pedagogical-projects"
+    component = { PedagogicalProjects }
+    /> <
+    Route path = "/professional"
+    component = { Professional }
+    /> <
+    Route path = "/professional-projects"
+    component = { ProfessionalProjects }
+    /> <
+    Route exact path = "/project/:index"
+    component = { Project }
+    /> <
+    Route path = "/tools"
+    component = { Tools }
+    /> <
+    Route component = { NotFound }
+    /> <
+    /Switch> <
+    /BrowserRouter>
 );
 
 export default Router;
