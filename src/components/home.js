612c3ef0e6e6923cba2a75dd9cf4175f558e4f16
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-11-07 14:41:22 +0100

    change refresh icon

--- src/components/home.js
+++ src/components/home.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
 
 
-class Home extends Component{
-    render(){
-        return(
-            <div className="ct-home">
-                <div>
-                    <span>P</span>
-                    <span>O</span>
-                    <span>R</span>
-                    <span>T</span>
-                    <span>F</span>
-                    <span>O</span>
-                    <span>L</span>
-                    <span>I</span>
-                    <span>O</span>
-                </div>
-                <p className="txt-name">María Alexandra Galarza Gallardo</p>
-            </div>
+class Home extends Component {
+    render() {
+        return ( <
+            div className = "ct-home" >
+            <
+            div >
+            <
+            span > P < /span> <
+            span > O < /span> <
+            span > R < /span> <
+            span > T < /span> <
+            span > F < /span> <
+            span > O < /span> <
+            span > L < /span> <
+            span > I < /span> <
+            span > O < /span> < /
+            div > <
+            p className = "txt-name" > María Alexandra Galarza Gallardo < /p> < /
+            div >
         );
     }
 }
 
 export default Home;
-
-
