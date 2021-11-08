56292152f435fc19e0120c68f9326cff0bb17c65
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-11-08 09:48:40 +0100

    spacing in portfolio letters

--- src/components/home.js
+++ src/components/home.js
@@ -7,18 +7,17 @@ class Home extends Component {
             div className = "ct-home" >
             <
             div >
-            <
-            span > P < /span> <
-            span > O < /span> <
-            span > R < /span> <
-            span > T < /span> <
-            span > F < /span> <
-            span > O < /span> <
-            span > L < /span> <
-            span > I < /span> <
-            span > O < /span> < /
-            div > <
-            p className = "txt-name" > María Alexandra Galarza Gallardo < /p> < /
+                <span>P</span>
+                <span>O</span>
+                <span>R</span>
+                <span>T</span>
+                <span>F</span>
+                <span>O</span>
+                <span>L</span>
+                <span>I</span>
+                <span>O</span>
+            </div >
+            <p className = "txt-name" > María Alexandra Galarza Gallardo < /p> < /
             div >
         );
     }
