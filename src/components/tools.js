a2e8772c73f2dfc685091fa62b62c2213339e745
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-11-08 12:52:14 +0100

    pedagogical info in json file, description klikshare, fixed

--- src/components/tools.js
+++ src/components/tools.js
@@ -21,7 +21,7 @@ class Tools extends Component{
                 <div className="ct-contentAbout">
                     <p className="txt-title">Technologies and Tools</p>
                     <p className="txt-paragraph">Before studing web development, I have carried out projects empirically linked to programming, as a UX/UI designer, systems conceptualization, and content management. Within the training in front-end and fullstack development that I have carried out at Factoría F5, Barcelona school, we have done pedagogical projects with agile methodologies and applying SOLID concepts developing with TDD and unit tests.<br/> 
-                    The technologies and tools that I have been using for development are Adobe Illustrator, Adobe Photoshop, Figma, Trello, Miró, HTML5, CSS3, Bootstrap, Tailwind, Javascript, Jest, Vue.js, Vuex, React.js, Sass, PHP, Laravel, PHPUnit , Xampp, MySQL, Visual Studio Code, Composer, Node.js, Github and more...</p>
+                    The technologies and tools that I have been using for development are Adobe Illustrator, Adobe Photoshop, Figma, Trello, Miró, HTML5, CSS3, Bootstrap, Tailwind, Javascript, Jest, Vue.js, Vuex, React.js, Sass, PHP, Laravel, PHPUnit, Xampp, MySQL, Visual Studio Code, Composer, Node.js, Github and more...</p>
                     <Tool/>
                 </div>
                 <div className="ct-divDown"><div className="img-div"></div></div>
