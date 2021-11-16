7de313793162c4fcf3d997453004f739152d8880
Alexandra Galarza <alexandragalarzag@gmail.com>
2021-11-16 12:53:59 +0100

    reorder empty card in cards.js

--- src/components/cards.js
+++ src/components/cards.js
@@ -6,10 +6,10 @@ class Cards extends Component{
     render(){
         return(
             <div className="ct-cards">
+                <EmptyCard/>
                 {this.props.data.projects.map((card, index) => (
                     <Card to={`/project/${index}`} key={index} title={card.title} subtitle={card.subtitle} description={card.description} team={card.team} img={card.img} prototype={card.prototype} website={card.website} repository={card.repository} index={index}></Card>
                 )).reverse()}
-                <EmptyCard/>
             </div>
         );
     }
