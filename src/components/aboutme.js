e759838848a51cbc2796d1397a8b9a55641f9404
MAGG <alexandragalarzag@gmail.com>
2022-02-22 12:56:00 +0100

    contact button in about me page

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import refresh from "../assets/img/refresh.svg";
+import contact from "../assets/img/contact.svg";
 
 class Aboutme extends Component {
 
@@ -18,13 +19,22 @@ class Aboutme extends Component {
         this.props.history.push("/");
     }
 
+    goHome = e => {
+        e.preventDefault();
+        this.props.history.push("/");
+    }
+
     render() {
         return (
             <div className = "ct-main" >
                 <div className="ct-divUp">
                     <div className="img-div"></div>
-                </div >
-                <img className="img-refresh" src={refresh} alt="refresh button" onClick={this.goHome} />
+                </div>
+                <div className="ct-navAbout">
+                   <img className="img-refresh" src={refresh} alt="refresh button" onClick={this.goHome} />
+                    <a href="mailto:alexandragalarzag@gmail.com"><img className="img-contact" src={contact} alt="contact button" onClick={this.goMail} /></a> 
+                </div>
+                
                 <div className = "ct-contentAbout">
                     <p className="txt-title">About Me</p>
                     <p className="txt-paragraph">I 'm a Fullstack developer creative and with initiative, interested in the world of programming, I'm passionate about solving problems in an efficient way and with a lot of emphasis on detail, I have more than 10 years of experience as an architect which gives me extensive knowledge in design and user experience, in addition to my studies in sustainability that allow me to understand current problems in a global way.< br />With these knowledge, skills and the <button className="bt-tools" onClick={this.goTools}><strong>tools</strong></button> I handle I feel that I can contribute to the needs of society through programming.</p>
