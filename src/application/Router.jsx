892cecb267476fe4169e2684dd3940313bb9407e
MAlexGG <alexandragalarzag@gmail.com>
2024-11-30 14:53:33 +0100

    about page responsive

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router"
 import App from "../App"
 import Aboutme from "../pages/aboutme/Aboutme"
 import Projects from "../pages/projects/Projects"
+import Tools from "../pages/tools/Tools"
 
 function Router() {
   return (
@@ -10,8 +11,8 @@ function Router() {
             <Route path="/" element={<App/>}/>
             <Route path="/aboutme" element={<Aboutme/>}/>
             <Route path="/projects" element={<Projects/>}/>
+            <Route path="/tools" element={<Tools/>}/>
             {/* <Route path="" element={}/>
-            <Route path="" element={}/>
             <Route path="" element={}/>
             <Route path="" element={}/>
             <Route path="" element={}/>
