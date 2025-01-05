ad31e4a14fe180a561a4835dd21315ba40bb95e0
MAlexGG <alexandragalarzag@gmail.com>
2025-01-05 23:52:53 +0100

    text from project updated

--- src/pages/project/Project.jsx
+++ src/pages/project/Project.jsx
@@ -11,12 +11,12 @@ function Project() {
     <div className={styles.ctProject}>
       <Link to={`/${data.category}`} className={styles.btProjectClose}><img src={close} alt="close button" /></Link>
       <div className={styles.ctProjectImg}>
-          <img className={styles.imgProjectImg} src={`/img/ui/${data.img}`} alt="design UI"/> 
+          <img className={styles.imgProjectImg} src={`/img/ui/${data.img}`} alt={`design UI or schema for ${data.title}`} /> 
       </div>
       <div className={styles.ctProjectTxt}>
           <h2 className={styles.txtProjectTitle}>{data.title}</h2>
           <p className={styles.txtProjectDesc}>{data.description}</p>
-          <p className={styles.txtProjectDesc}><strong>Team:</strong> {data.team}</p>
+          <p className={`${styles.txtProjectDesc} ${styles.txtItalic}`}><strong>Authors:</strong> {data.team}</p>
       </div>
     </div>
   )
