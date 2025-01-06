83e8e864b2fa0993643e1a9474ccb0ebb6ce8dbb
MAlexGG <alexandragalarzag@gmail.com>
2025-01-06 02:09:03 +0100

    vite config updated

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://malexgg.github.io/portfolio/"
+  base: "https://github.com/malexgg/portfolio",
 })
