65c857f2d2e50f5eb31d29cab3f9b63abb6a84ca
MAlexGG <alexandragalarzag@gmail.com>
2026-02-08 22:13:15 +0100

    icons with label in both languages

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,15 +6,19 @@ import green from "/img/icons/green.svg";
 import styles from './Navbar.module.css'
 import InternalLink from '../internalLink/InternalLink';
 import LangToggle from '../langToggle/LangToggle';
+import { translations } from '../../translations'
 
 function Navbar({background}) {
 
+  const { language } = useLanguage();
+  const t = translations[language].nav;
+
   return (
     <nav className={background == "dark" ? `${styles.ctNav} ${styles.dark}`: styles.ctNav}>
-        <InternalLink link="/aboutme" image={home} alt="navigation to about me page" text="home" background={background}/>
-        <InternalLink link="/projects" image={projects} alt="navigation to projects page" text="projects" background={background}/>
-        <InternalLink link="/tools" image={tools} alt="navigation to tools page" text="tools" background={background}/>
-        <InternalLink link="/sustainability" image={green} alt="navigation to green page" text="green" background={background}/>
+        <InternalLink link="/aboutme" image={home} alt="navigation to about me page" text={t.home} background={background}/>
+        <InternalLink link="/projects" image={projects} alt="navigation to projects page" text={t.projects} background={background}/>
+        <InternalLink link="/tools" image={tools} alt="navigation to tools page" text={t.tools} background={background}/>
+        <InternalLink link="/sustainability" image={green} alt="navigation to green page" text={t.green} background={background}/>
         <div className={styles.ctLangToggle}>
           <LangToggle/>
         </div>
@@ -22,5 +26,6 @@ function Navbar({background}) {
     </nav>
   )
 }
+import { useLanguage } from '../../context/LanguageContext';
 
 export default Navbar
