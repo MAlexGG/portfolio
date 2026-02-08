d250f03100a1b6fac09beb83c284999ac07e19f1
MAlexGG <alexandragalarzag@gmail.com>
2026-02-08 20:17:09 +0100

    about me page content with both languages

--- src/pages/aboutme/Aboutme.jsx
+++ src/pages/aboutme/Aboutme.jsx
@@ -19,19 +19,23 @@ function Aboutme() {
   }
 
   const {language} = useLanguage();
-  const t = translations[language];
+  const t = translations[language].aboutme;
 
   return (
     <div className={styles.ctAboutme}>
         <Divisor position="up"/>
         <Contact/>
         <main className={styles.ctContentAbout}>
-          <p className={styles.txtTitle}>{t.aboutme.title}</p>
-          <p className={styles.txtParagraph}>Hi! I&apos;m Alexandra and I&apos;m a Full-Stack Developer with a strong foundation in architecture and project management. My journey from designing physical spaces to creating digital products has strengthened my analytical thinking, creativity, and user-centered mindset. I enjoy building clean, maintainable web applications using modern technologies.</p>
-          <p className={styles.txtParagraph}>Curious and continuous learner, I&apos;m passionate about technology that improves people&apos;s lives. I aim to contribute what I&apos;ve learned both technically and personally to build meaningful, efficient, and sustainable digital products.</p>
-          <p className={styles.txtParagraph}>With these knowledge, skills and the <button className={styles.btTools} onClick={goTools}><strong>tools</strong></button> I handle I feel that I can contribute to the needs of society through development.</p>
-          <Button text="PROJECTS" style="btStroke" onclick={handleClick}/>
-          <h1 className={styles.txtMain}>María Alexandra Galarza Gallardo&apos;s Portfolio</h1>
+          <p className={styles.txtTitle}>{t.title}</p>
+          <p className={styles.txtParagraph}>{t.paragraph_one}</p>
+          <p className={styles.txtParagraph}>{t.paragraph_two}</p>
+          <p className={styles.txtParagraph}>{t.paragraph_three_start}
+            <button className={styles.btTools} onClick={goTools}>
+              <strong>{t.tools_button}</strong>
+            </button>
+          {t.paragraph_three_end}</p>
+          <Button text={t.projects_button} style="btStroke" onclick={handleClick}/>
+          <h1 className={styles.txtMain}>{t.h1}</h1>
         </main>
       <Divisor position="down"/>
     </div>
